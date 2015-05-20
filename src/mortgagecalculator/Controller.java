e7e64c7198593331f0db407d9f694891c8bea6f8
Robert Furlong <stefurlr@udel.edu>
2015-05-20 00:19:16 -0400

    final exam

---
+++ src/mortgagecalculator/Controller.java
@@ -0,0 +1,23 @@
+package mortgagecalculator;
+
+public class Controller {
+
+	public static void DropBox(int Term){
+		insert (int 10, int 15, int 30)
+	}
+	public static void calculations(){
+		textfield=(.18*gross income)
+	}
+	public static void calculations2(){
+		textfield=(.36*gross income)
+	}
+	public static void calculations3(){
+		if (calculations < calculations2 
+				then textfield=calculations
+				elsif calculations2 < calculations
+				then textfield=calculations2)
+	}
+	public static void calculations4(){
+		textfield=((interestrate/12)*(12*term)*(Calculations3))
+	}
+	}
